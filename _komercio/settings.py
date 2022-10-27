4c5b89a590d455062ad0e9c021be2231b13460e5
PandaSuzuki <renatosuzuki03@gmail.com>
2022-10-27 03:43:42 -0300

    Changed ALLOWED_HOSTS in settings.py

--- _komercio/settings.py
+++ _komercio/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ["komercio-renatosuzuki.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["komercio-renatosuzuki.herokuapp.com/", "localhost"]
 
 
 # Application definition
